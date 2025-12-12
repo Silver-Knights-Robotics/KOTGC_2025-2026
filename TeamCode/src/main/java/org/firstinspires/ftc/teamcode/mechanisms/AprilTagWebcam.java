a27cbed7d75a7dbd4ab84a7fc37b31689e20b742
Slothjira <marcusarguello54@gmail.com>
2025-12-11 17:27:03 -0700

    Create AprilTagWebcam.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagWebcam.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AprilTagWebcam {
+
+    private AprilTagProcessor aprilTagProcessor;
+private VisionPortal visionPortal;
+
+private List<AprilTagDetection> detectedTags = new ArrayList<>();
+
+}
