475bb6429d6928d8e77597f1625626869c382080
Slothjira <marcusarguello54@gmail.com>
2025-12-11 17:30:11 -0700

    Update AprilTagWebcam.java
    Added new shi
    
    Created new mechanisms and april tag stuff
    
    Type shi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagWebcam.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/AprilTagWebcam.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.mechanisms;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
@@ -13,5 +16,13 @@ public class AprilTagWebcam {
 private VisionPortal visionPortal;
 
 private List<AprilTagDetection> detectedTags = new ArrayList<>();
+
+private Telemetry telemetry;
+
+public void init(HardwareMap haMap, Telemetry telemetry) {
+
+
+
+}
 
 }
